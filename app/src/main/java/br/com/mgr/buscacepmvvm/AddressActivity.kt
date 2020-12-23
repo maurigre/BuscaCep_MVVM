b7a01b399cc8d3b16f6ac5532ee2a201c2f5e57e
Mauri <maurigre@gmail.com>
2020-12-23 15:32:54 -0300

    Tratamento de retorno de erro da API na aplicação
    Took 3 hours 24 minutes

---
+++ app/src/main/java/br/com/mgr/buscacepmvvm/AddressActivity.kt
@@ -0,0 +1,59 @@
+package br.com.mgr.buscacepmvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
+import android.widget.Toast.LENGTH_SHORT
+import androidx.lifecycle.Observer
+import br.com.mgr.buscacepmvvm.address.Address
+import br.com.mgr.buscacepmvvm.address.AddressViewModel
+import br.com.mgr.buscacepmvvm.address.AddressViewState
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class AddressActivity : AppCompatActivity() {
+
+    lateinit var cepTextView: TextView
+    lateinit var enderecoTextView: TextView
+    lateinit var cidadeTextView: TextView
+    lateinit var ufTextView: TextView
+
+    private val addressViewModel: AddressViewModel by viewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        cepTextView = findViewById(R.id.cepTextView)
+        enderecoTextView = findViewById(R.id.enderecoTextView)
+        cidadeTextView = findViewById(R.id.cidadeTextView)
+        ufTextView = findViewById(R.id.ufTextView)
+
+        cepTextView.setOnFocusChangeListener { view, hasFocus ->
+            if(!hasFocus && cepTextView.text.isNotEmpty()){
+                addressViewModel.getAddress(cepTextView.text.toString())
+            }
+        }
+
+        //observar LiveData da AddressViewModel
+        addressViewModel.liveData.observe(this, Observer { state ->
+            when(state){
+                is AddressViewState.Loading -> Toast.makeText(this,"Iniciando consulta", LENGTH_SHORT).show()
+                is AddressViewState.Success -> preencheCampos(state.address)
+                is AddressViewState.Error -> showError(state.throwable)
+            }
+        })
+    }
+
+    fun preencheCampos(address: Address){
+        enderecoTextView.text = address.street
+        cidadeTextView.text = address.city
+        ufTextView.text =address.state
+    }
+
+    fun showError(throwable: Throwable){
+        preencheCampos(Address())
+        Toast.makeText(this,"CEP informado não encontrado.",LENGTH_LONG).show()
+    }
+}
--- app/src/main/java/br/com/mgr/buscacepmvvm/address/AddressViewModel.kt
+++ app/src/main/java/br/com/mgr/buscacepmvvm/address/AddressViewModel.kt
@@ -1,26 +1,49 @@
 package br.com.mgr.buscacepmvvm.address
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import br.com.mgr.buscacepmvvm.address.AddressViewState.Error
+import br.com.mgr.buscacepmvvm.address.AddressViewState.Success
 import br.com.mgr.buscacepmvvm.service.BrasilApiService
+import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.IOException
+
 
 class AddressViewModel(
     private val service: BrasilApiService
 ): ViewModel() {
 
     //Variavel que sera observada pela Activity
-    private val _livedata = MutableLiveData<Address>()
-    val liveData: LiveData<Address> = _livedata
+    private val _livedata = MutableLiveData<AddressViewState>()
+    val liveData: LiveData<AddressViewState> = _livedata
 
-    fun getAddress(cep: String){
-        viewModelScope.launch(Dispatchers.IO){
-            val result: Address = service.getAddressByCep(cep)
-            _livedata.postValue(result)
-        }
+
+    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        _livedata.value = Error(throwable)
     }
 
+    fun getAddress(cep: String){
+        _livedata.value = AddressViewState.Loading
+
+        viewModelScope.launch(exceptionHandler) {
+            withContext(Dispatchers.IO) {
+                val result: Address = service.getAddressByCep(cep)
+                _livedata.postValue( Success(result))
+             }
+        }
+    }
+}
+
+sealed class AddressViewState {
+    object Loading : AddressViewState()
+    data class Error(val throwable: Throwable) : AddressViewState()
+    data class Success(val address: Address) : AddressViewState()
 }
